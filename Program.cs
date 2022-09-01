49faa0289231113c8b5d7e3a80e7a341ccfff8b0
Adeshina <adeshiname@gmail.com>
2022-09-01 20:50:18 +0100

    Optimizations

--- Program.cs
+++ Program.cs
@@ -12,30 +12,45 @@ namespace Rooster
         public class Tag
         {
             public int Id { get; set; }
+            public int Prev { get; set; }
+            public int Current { get; set; }
             public string User { get; set; }
             public DayOfWeek Day { get; set; }
             public DateTime Date { get; set; }
         }
 
+        static int IndexGenerator(int length, int prev,int listLength)
+        {
+            var random = new Random();
+            var index = random.Next(length);
+            if (listLength < 1)
+            {
+                return index;
+            }
+            if(index == prev)
+            {
+                return IndexGenerator(length, prev, listLength);
+                
+            }
+            return index;
+        }
+
+
         static void Main(string[] args)
         {
-
-          
-            var Users = new List<string>() { "User A","User B","User C","User D",
-            "Mich","Victor Usoro","Paul Odeh","Adeshina Adebayo","Abiodun Adegbuyi","Andrew Ehikioya"};
+            var Users = new List<string>() { "Immaculate Egwu","Kalu Orji","Elsie Okorochukwu","Abayomi Omosehin",
+            "Micheal Madume","Victor Usoro","Paul Odeh","Adesina Adebayo","Abiodun Adegbuyi","Andrew Ehikioya"};
             var Months = new List<string>();
             var meetingDays = new List<DayOfWeek> { (DayOfWeek)1, (DayOfWeek)3, (DayOfWeek)5 };
             var now = DateTime.Now;
             var response = new List<Tag>();
             var daysInMonth = DateTime.DaysInMonth(now.Year, now.Month);
-            Console.WriteLine("{0} Days",daysInMonth);
-            Console.WriteLine(now.Date.Day);
-            var random = new Random();
+            int prevIndex = 0;
             for (int i = 1; i < daysInMonth + 1; i++)
             {
                 var t = new Tag();
                 var day = new DateTime(now.Year, now.Month, i);
-                var index = random.Next(Users.Count);
+                var index = IndexGenerator(Users.Count, prevIndex, response.Count);
                 var dayOfWeek = day.DayOfWeek;
                 //When it matches any day in meeting days
                 if (meetingDays.Contains(dayOfWeek))
@@ -44,14 +59,15 @@ namespace Rooster
                     t.User = Users[index];
                     t.Day = dayOfWeek;
                     t.Id = i;
+                    t.Current = index;
+                    t.Prev = prevIndex;
                     response.Add(t);
-                   
+                    prevIndex = index;
                 }
             }
             foreach (var item in response)
             {
                 Console.WriteLine("Day: {0} Date : {1}, Cordinator : {2}",item.Day, item.Date, item.User);
-               
             }
 
 
